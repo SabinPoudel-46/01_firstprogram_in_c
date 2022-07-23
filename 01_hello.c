e39bce312366aa54e9626544fe0d02ecd80a4209
SabinPoudel-46 <91828300+SabinPoudel-46@users.noreply.github.com>
2022-07-23 07:44:09 +0530

    Add files via upload

---
+++ 01_hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h> //this is preprocessor directive to include standard input output files
+int main () // main is a function from where execution of program takes place and int is return type
+{
+	printf("Hello World"); //printf is a inbuilt function under stdio.h header file for output
+	
+	return 0; //since the return type is integer returning 0 to operating system
+}
